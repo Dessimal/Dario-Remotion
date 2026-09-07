99b2d37fb36d75dc6ca3c28f1bf04029c3188941
Dessimal <65266493+Dessimal@users.noreply.github.com>
2026-09-07 17:09:57 +0100

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,7 +3,7 @@ import { Composition } from "remotion";
 import { HelloWorld } from "./HelloWorld";
 import { Logo } from "./HelloWorld/Logo";
 import { VoiceoverScene } from "./compositions/VoiceoverScene";
-
+import { Scene01_Intro } from './scenes/Scene01_Intro';
 // Each <Composition> is an entry in the sidebar!
 
 export const RemotionRoot: React.FC = () => {
@@ -36,6 +36,15 @@ export const RemotionRoot: React.FC = () => {
         width={1920}
         height={1080}
       />
+
+      <Composition
+  id="Scene01Intro"
+  component={Scene01_Intro}
+  durationInFrames={534} // 17.8s hook, at 30fps — adjust once you preview
+  fps={30}
+  width={1920}
+  height={1080}
+/>
 
       {/* Mount any React component to make it show up in the sidebar and work on it individually! */}
       <Composition
