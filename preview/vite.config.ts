2213a85d63a3262e3cbd5dc716dae105acbf4243
Dessimal <65266493+Dessimal@users.noreply.github.com>
2026-09-08 09:53:38 +0100

    Add publicDir configuration to Vite config

--- preview/vite.config.ts
+++ preview/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: path.resolve(__dirname, '../public'),
   resolve: {
     alias: {
       remotion: path.resolve(__dirname, 'node_modules/remotion'),
