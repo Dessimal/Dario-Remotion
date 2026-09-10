b2684c70da1e58950043e96831561b852ee9c666
Dessimal <65266493+Dessimal@users.noreply.github.com>
2026-09-10 11:32:28 +0100

    Delete src/components/Scene background.tsx

--- src/components/Scene background.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { AbsoluteFill, Img, staticFile } from 'remotion';
-
-export const SceneBackground: React.FC = () => (
-  <Img
-    src={staticFile('backgrounds/background.jpeg')}
-    style={{ width: '100%', height: '100%', objectFit: 'cover', position: 'absolute' }}
-  />
-);
